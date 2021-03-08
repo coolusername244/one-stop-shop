664bf3d1bc1be3b65445f02296d254bca8448d05
coolusername244 <leesheppard2404@gmail.com>
2021-03-08 11:37:22 +0000

    Customised the django admin panel for products and categories

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'name',
+        'price',
+        'rating',
+        'image',
+        'sku',
+    )
+
+    ordering = ('category',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
