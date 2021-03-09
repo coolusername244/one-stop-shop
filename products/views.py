83bd03782b14164b59d6645191f91eda03ab6db7
coolusername244 <leesheppard2404@gmail.com>
2021-03-09 12:20:33 +0000

    added search functionality based on words in products.json

--- products/views.py
+++ products/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
 def products_library(request):
     # This is the view that will return all of the products
-    # and have sorting functions
+    # and have sorting and searching functions
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'query' in request.GET:
+            query = request.GET['query']
+            if not query:
+                messages.error(request, "One must know what one is looking for")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
