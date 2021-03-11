1f48917b54283599c09f612aa7f62ee33a713a7e
coolusername244 <leesheppard2404@gmail.com>
2021-03-11 19:19:12 +0000

    added 'modify_bag' to urls.py

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.view_shopping_bag, name='view_shopping_bag'),
     path('add/<item_id>', views.add_item_to_bag, name='add_item_to_bag'),
+    path('modify/<item_id>', views.modify_bag, name='modify_bag'),
 ]
