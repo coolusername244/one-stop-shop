81b8f3364bf932977a114546ca40b3eadf89783a
coolusername244 <leesheppard2404@gmail.com>
2021-03-13 15:48:59 +0000

    added crispy forms to settings.py

--- MS4/settings.py
+++ MS4/settings.py
@@ -82,6 +82,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents'
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
