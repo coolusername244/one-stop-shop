acabbfbd1eee41bd35b40fa71a67fbe011378ee5
coolusername244 <leesheppard2404@gmail.com>
2021-03-13 15:47:41 +0000

    added crispy forms to settings.py

--- MS4/settings.py
+++ MS4/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +61,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'MS4.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
