3699d04e4e3adf41d0dc3a8e979edadbb4e44723
coolusername244 <leesheppard2404@gmail.com>
2021-03-16 13:50:26 +0000

    added secret and public key for stripe to settings.py

--- MS4/settings.py
+++ MS4/settings.py
@@ -171,3 +171,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
