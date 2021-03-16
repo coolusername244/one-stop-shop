363b46e64b8600878bc21a9ec7faff19334fe50e
coolusername244 <leesheppard2404@gmail.com>
2021-03-16 18:01:05 +0000

    added cache checkut data url

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,5 +5,6 @@ from .webhooks import webhook
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_complete/<order_number>', views.checkout_complete, name='checkout_complete'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
 ]
