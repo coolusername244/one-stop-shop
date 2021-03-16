a1b767e9505ed951f49ab88b54f3626ad5c1faec
coolusername244 <leesheppard2404@gmail.com>
2021-03-16 17:40:18 +0000

    added webhook handler urls

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_complete/<order_number>', views.checkout_complete, name='checkout_complete'),
+    path('wh/', webhook, name='webhook'),
 ]
