b64e11d957b04fd4e3f681643026c9bbc96d10b2
coolusername244 <leesheppard2404@gmail.com>
2021-03-16 18:01:56 +0000

    added webhooks to catch the payment intent order form

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
 import stripe
+import json
 import os
 
 from os import path
@@ -14,6 +16,23 @@ from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
 from bag.contexts import bag_contents
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -15,6 +15,8 @@ class StripeWH_Handler:
 
     def handle_payment_intent_succeeded(self, event):
         # Handle the payment_intent.succeeded webhook from Stripe
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
