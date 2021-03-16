2885f3aa5bc36327a43bdc7b8118e1178cd730e5
coolusername244 <leesheppard2404@gmail.com>
2021-03-16 16:56:49 +0000

    added urls for checkout complete

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    path('', views.checkout, name='checkout'),
+    path('checkout_complete/<order_number>', views.checkout_complete, name='checkout_complete'),
 ]
