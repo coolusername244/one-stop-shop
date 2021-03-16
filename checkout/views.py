05b804d86969d6a8549a1f23b4e7baee63ce7470
coolusername244 <leesheppard2404@gmail.com>
2021-03-16 13:52:53 +0000

    added stripe info to views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,43 @@
+import stripe
+import os
+
+from os import path
+
+if path.exists('env.py'):
+    import env
+
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
 
 
 def checkout(request):
+    stripe_public_key = os.environ.get('STRIPE_PUBLIC_KEY')
+    stripe_secret_key = os.environ.get('STRIPE_SECRET_KEY')
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing to see here!")
         return redirect(reverse('products'))
+    current_bag = bag_contents(request)
+    total = current_bag['total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51IUbcLFctfkBQlvnBFXGupaHPdNtCfrNBMIB8lMmg6xxs303LxVLKcPK2T7xJkDMfMxy1xa9ZFHMxG8iBWVi6Qzp00FJWoThu7',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret
     }
 
     return render(request, template, context)
