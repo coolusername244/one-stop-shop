7d16cb9ba834879ffe7c88ef795f26a0026fc744
coolusername244 <leesheppard2404@gmail.com>
2021-03-19 13:20:57 +0000

    added logic to update form data on click

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully!')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/user-profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
+        'profile_page': True,
     }
 
     return render(request, template, context)
