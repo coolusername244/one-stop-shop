3b2d69f90af606fe252c9876ff2b0a9da7f850f4
coolusername244 <leesheppard2404@gmail.com>
2021-03-19 15:33:18 +0000

    added order history method for user to see past confirmations

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
 
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -22,6 +24,23 @@ def profile(request):
         'form': form,
         'orders': orders,
         'profile_page': True,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f" This is a past confirmation for order number {order_number}."
+        "A confirmation email was sent on the order date. "
+    ))
+
+    template = 'checkout/checkout_complete.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
