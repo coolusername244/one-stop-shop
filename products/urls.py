441db17f8fd4fadc5b0fcc641a7422cb1511eb70
coolusername244 <leesheppard2404@gmail.com>
2021-03-19 17:30:45 +0000

    added edit product url

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.products_library, name='products'),
     path('<int:product_id>/', views.single_product_detail, name='single_product_detail'),
     path('add/', views.add_a_product, name='add_a_product'),
+    path('edit/<int:product_id>/', views.edit_a_product, name='edit_a_product'),
 ]
