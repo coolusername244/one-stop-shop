84ba71889af5eb143971e68dbd2ddc17bed4e86b
coolusername244 <leesheppard2404@gmail.com>
2021-03-19 17:36:13 +0000

    added url for deleting products

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:product_id>/', views.single_product_detail, name='single_product_detail'),
     path('add/', views.add_a_product, name='add_a_product'),
     path('edit/<int:product_id>/', views.edit_a_product, name='edit_a_product'),
+    path('delete/<int:product_id>/', views.delete_a_product, name='delete_a_product'),
 ]
