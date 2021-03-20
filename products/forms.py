bb26df27edd973842de9d5269c4e97793b67134a
coolusername244 <leesheppard2404@gmail.com>
2021-03-20 17:24:53 +0000

    added widget to product form

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 class ProductForm(forms.ModelForm):
@@ -6,7 +7,9 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
-    
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
