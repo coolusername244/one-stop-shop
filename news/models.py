51b56769c0fdb4d8cdaba86f95bb9331a0b33058
coolusername244 <leesheppard2404@gmail.com>
2021-03-22 15:42:08 +0000

    made basic news model and migrated changes

--- news/models.py
+++ news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=80)
+    body = models.TextField()
+    # image
+    date = models.DateTimeField(auto_now_add=True)
+    # author
