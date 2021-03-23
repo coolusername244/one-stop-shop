4845b5e04bdf3c29c80e01b2ca6fb60f93192a2d
coolusername244 <leesheppard2404@gmail.com>
2021-03-23 11:15:23 +0000

    added image and image url fields to news model

--- news/models.py
+++ news/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 
 class News(models.Model):
+
     title = models.CharField(max_length=80)
     body = models.TextField()
-    # image
+    image = models.ImageField(null=True,
+                              blank=True)
+    image_url = models.URLField(max_length=1024,
+                                null=True,
+                                blank=True)
     date = models.DateTimeField(auto_now_add=True)
     # author
+
+    class Meta:
+        verbose_name_plural = 'News'
+
+    def __str__(self):
+        return self.title
