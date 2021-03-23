11829cb6dc41d7f0ca2e72fce3aba0e5e5edf243
coolusername244 <leesheppard2404@gmail.com>
2021-03-23 14:52:23 +0000

    added view for user to send messages to admin

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from . import forms
 
 
 def contact(request):
+
+    if request.method == 'POST':
+        contact_form = forms.ContactForm(request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.success(request,
+                             'Your message has been sent to our admin!')
+            return redirect('home')
+    else:
+        contact_form = forms.ContactForm()
+
     template = 'contact/contact.html'
-    return render(request, template)
+    context = {
+        'contact_form': contact_form
+    }
+
+    return render(request, template, context)
