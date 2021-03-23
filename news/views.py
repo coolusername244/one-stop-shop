131e2c3709f78b99094e54e6d7c91f1bcbe1c376
coolusername244 <leesheppard2404@gmail.com>
2021-03-23 11:10:55 +0000

    added views for users to see news and superusers to add news

--- news/views.py
+++ news/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from . import forms
+from .models import News
 
 
 def news(request):
+
+    news = News.objects.all().order_by('date')
+
     template = 'news/news.html'
-    return render(request, template)
+    context = {
+        'news': news,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def add_news(request):
+    add_news_form = forms.NewsForm()
+
+    template = 'news/add_news.html'
+    context = {
+        'add_news_form': add_news_form,
+    }
+
+    return render(request, template, context)
