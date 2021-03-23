0aed9d6410543fb4810a371c24b9f1d538de4534
coolusername244 <leesheppard2404@gmail.com>
2021-03-23 12:58:58 +0000

    Updated view 'add_news' to post data to db from form

--- news/views.py
+++ news/views.py
@@ -7,7 +7,7 @@ from .models import News
 
 def news(request):
 
-    news = News.objects.all().order_by('date')
+    news = News.objects.all().order_by('-date')
 
     template = 'news/news.html'
     context = {
@@ -19,7 +19,20 @@ def news(request):
 
 @login_required
 def add_news(request):
-    add_news_form = forms.NewsForm()
+
+    if not request.user.is_superuser:
+        messages.error(request,
+                       'You do not have permission to carry out this task.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        add_news_form = forms.NewsForm(request.POST, request.FILES)
+        if add_news_form.is_valid():
+            add_news_form.save()
+            messages.success(request, 'News article added successfully!')
+            return redirect('news')
+    else:
+        add_news_form = forms.NewsForm()
 
     template = 'news/add_news.html'
     context = {
