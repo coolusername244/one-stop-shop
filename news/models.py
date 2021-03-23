42408b0471ff36159dd4f31a2f38625996042084
coolusername244 <leesheppard2404@gmail.com>
2021-03-23 12:57:22 +0000

    added author to news model and migrated

--- news/models.py
+++ news/models.py
@@ -1,17 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class News(models.Model):
 
     title = models.CharField(max_length=80)
     body = models.TextField()
-    image = models.ImageField(null=True,
-                              blank=True)
-    image_url = models.URLField(max_length=1024,
-                                null=True,
-                                blank=True)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    # author
+    author = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = 'News'
