64210b080c6c483c0176c641c17034d41a3121d8
coolusername244 <leesheppard2404@gmail.com>
2021-03-23 14:53:36 +0000

    added model for contact and migrated changes

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    subject = models.CharField(max_length=80)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Contact'
