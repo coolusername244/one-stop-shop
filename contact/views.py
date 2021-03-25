51483d345bd3e1bd48c1556ddf68643235dc811f
coolusername244 <leesheppard2404@gmail.com>
2021-03-25 14:16:59 +0000

    changed success message to info message to stop showing basket loop

--- contact/views.py
+++ contact/views.py
@@ -9,8 +9,8 @@ def contact(request):
         contact_form = forms.ContactForm(request.POST)
         if contact_form.is_valid():
             contact_form.save()
-            messages.success(request,
-                             'Your message has been sent to our admin!')
+            messages.info(request,
+                          'Your message has been sent to our admin!')
             return redirect('home')
     else:
         contact_form = forms.ContactForm()
