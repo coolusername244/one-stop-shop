c7a14f0273f42b2db51e9c77854368e03db81ac1
coolusername244 <leesheppard2404@gmail.com>
2021-03-25 11:49:57 +0000

    removed comments

--- home/views.py
+++ home/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
 
-# Create your views here.
 def index(request):
-    # This is a view that returns index.html
-
     return render(request, 'home/index.html')
