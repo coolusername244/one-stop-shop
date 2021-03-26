5f4aa7c86dbf9d9cacb71df25e7c1ff2fad76a31
coolusername244 <leesheppard2404@gmail.com>
2021-03-26 14:00:53 +0000

    added edit post url

--- news/urls.py
+++ news/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
          views.add_news,
          name='add_news'
          ),
+    path('edit_news/<int:news_id>/',
+         views.edit_news,
+         name='edit_news'
+         )
 ]
