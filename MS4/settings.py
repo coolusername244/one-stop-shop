16c7f2b46d26b485ab12a071fd465e6510fa6d61
coolusername244 <leesheppard2404@gmail.com>
2021-03-27 12:36:07 +0000

    Changed allowed hosts and databases ready for deployment

--- MS4/settings.py
+++ MS4/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,7 @@ SECRET_KEY = 'k4yc52!3-$g=*=h!b-8=0i^dlk%9oz!019%35z(ln()fcjw2r0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coolusername244-one-stop-shop', 'localhost']
 
 
 # Application definition
@@ -125,12 +126,17 @@ WSGI_APPLICATION = 'MS4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn MS4.wsgi:application
